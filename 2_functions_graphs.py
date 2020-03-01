dbadf354368f06bc5561073391760f87d1094db4
Timofey Khirianov <tkhirianov@gmail.com>
2020-03-01 18:03:30 +0300

    some additions

--- 2_functions_graphs.py
+++ 2_functions_graphs.py
@@ -2,10 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-x = np.arange(-10, 10.01, 0.01)
-plt.plot(x, np.sin(x), x, np.cos(x), x, -x)
+x = np.arange(-10, 10.01, 0.1)
+plt.plot(x, np.sin(x),
+         x, np.cos(x),
+         [1, 2, 3, 4, 5],
+         [3, 5, 2, 5, 4])
 plt.xlabel(r'$x$')
 plt.ylabel(r'$f(x)$')
 plt.title(r'$f_1(x)=\sin(x),\ f_2(x)=\cos(x),\ f_3(x)=-x$')
 plt.grid(True)
+plt.savefig('1.png')
 plt.show()
